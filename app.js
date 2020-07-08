fb2856287511b2c733a7b9fc51e08734741dc0dc
Mykhailo Mokhoruk <Michael119Nr@mail.com>
2020-07-08 20:28:10 +0300

    project editing 1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 let model = {
   items: [
@@ -25,25 +25,7 @@ let permissionApp = angular.module("permissionApp", []);
 permissionApp.controller("permissionController", function ($scope) {
   $scope.list = model;
 
-  function getLocalStorage() {
-    try {
-      const serializedState = localStorage.getItem("permission");
-      if(serializedState) $scope.list.items = JSON.parse(serializedState);
-    } catch (err) {
-      console.error("Get state error: ", err);
-    }
-  };
-
   getLocalStorage();
-
-  $scope.addLocalStorage = function () {
-    try {
-      const serializedState = JSON.stringify($scope.list.items);
-      localStorage.setItem("permission", serializedState);
-    } catch (err) {
-      console.error("Set state error: ", err);
-    }
-  };
 
   $scope.checkAllView = function () {
     return $scope.list.items.every((obj) => obj.permission.view);
@@ -57,12 +39,21 @@ permissionApp.controller("permissionController", function ($scope) {
     return $scope.list.items.every((obj) => obj.permission.remove);
   };
 
-  $scope.checkSomeView = function () {
-    return $scope.list.items.some((obj) => obj.permission.view);
+  $scope.handleView = function (evt) {
+    let section = evt.target.id.slice(0, -4);
+    $scope.list.items.some((obj) => {
+      if (obj.section == section) {
+        obj.permission.edit = false;
+        obj.permission.remove = false;
+      }
+    });
   };
 
-  $scope.checkSomeEdit = function () {
-    return $scope.list.items.some((obj) => obj.permission.edit);
+  $scope.handleEdit = function (evt) {
+    let section = evt.target.id.slice(0, -4);
+    $scope.list.items.some((obj) =>
+      obj.section == section ? (obj.permission.remove = false) : null
+    );
   };
 
   $scope.checkedColumn = function (evt) {
@@ -70,15 +61,25 @@ permissionApp.controller("permissionController", function ($scope) {
     let keyPer = evt.target.id.slice(3);
 
     $scope.list.items.forEach((obj) => {
-      if (keyPer != "View") {
-        let elmCheckbox = document.querySelector(
-          `#${obj.section}${keyPer}`
-        );
-        if (!elmCheckbox.hasAttribute("disabled"))
-          obj.permission[keyPer.toLowerCase()] = true;
-      } else {
-        obj.permission[keyPer.toLowerCase()] = true;
-      }
+      obj.permission[keyPer.toLowerCase()] = true;
     });
+  };
+
+  function getLocalStorage() {
+    try {
+      const serializedState = localStorage.getItem("permission");
+      if (serializedState) $scope.list.items = JSON.parse(serializedState);
+    } catch (err) {
+      console.error("Get state error: ", err);
+    }
+  }
+
+  $scope.addLocalStorage = function () {
+    try {
+      const serializedState = JSON.stringify($scope.list.items);
+      localStorage.setItem("permission", serializedState);
+    } catch (err) {
+      console.error("Set state error: ", err);
+    }
   };
 });
